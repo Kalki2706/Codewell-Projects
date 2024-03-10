548fbbd99c34979f26b7b7e2dd285b19fc4ca4bd
Kalki <kalkithewarrior2706@proton.me>
2024-03-10 06:49:12 +0530

    initial commit

--- IndieBrew-Landing-Page/src/main.js
+++ IndieBrew-Landing-Page/src/main.js
@@ -3,43 +3,25 @@ const menu = document.querySelector('.menu'),
   nav = document.querySelector('.navbar'),
   buttons = document.querySelectorAll('button');
 
-function addClickEvent(selector, EventCallback) {
-  if (!(EventCallback instanceof Function)) {
-    console.error(
-      `Error: addClickEvent(selector, EventCallback): Second argument require to be a function callback but it is of ${typeof EventCallback} type`
-    );
-  }
-
-  let element = document.querySelectorAll(selector);
-  if (element.length === 1) {
-    console.log(element[0]);
-    element[0].addEventListener('click', EventCallback);
-  }
-
-  element.forEach((allElements) => {
-    allElements.addEventListener('click', EventCallback);
-  });
-}
-
-// menu
-addClickEvent('.menu', () => {
+menu.addEventListener('click', () => {
   nav.classList.add('open__nav');
   menu.style.display = 'none';
   close.style.display = 'block';
 });
 
-// close
-addClickEvent('.close', () => {
+close.addEventListener('click', () => {
   nav.classList.remove('open__nav');
   menu.style.display = 'block';
   close.style.display = 'none';
 });
 
-// buttons
-addClickEvent('button', () => {
-  function redirect() {
-    window.location.href = '../sign-up-page/index.html';
-  }
+// Sign Up page redirection
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    function redirect() {
+      window.location.href = './sign-up-page/index.html';
+    }
 
-  redirect();
+    redirect();
+  });
 });
